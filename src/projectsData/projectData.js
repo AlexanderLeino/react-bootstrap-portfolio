0b12929fb6f647343d389924d7433d7b7000cb92
Alexander Leino <Alexander.Leino@yahoo.com>
2022-11-30 16:37:21 -0500

    pdated project descriptions

--- src/projectsData/projectData.js
+++ src/projectsData/projectData.js
@@ -2,13 +2,10 @@ import { ImHtmlFive } from "react-icons/im";
 import { DiCss3 } from "react-icons/di";
 import {
   SiJavascript,
-  SiJquery,
   SiGraphql,
   SiReact,
-  SiMysql,
   SiMongodb,
   SiExpress,
-  SiHandlebarsdotjs,
   SiChakraui,
 } from "react-icons/si";
 import { FaNode } from "react-icons/fa";
@@ -34,7 +31,7 @@ export const projectsArray = [
   {
     title: "Black Jack",
     description:
-      "The goal of the project was to focus on writing dense programming logic when I first started writing code as well as focus on heavy DOM manipulation by creating each individual card using BootStrap, CSS, vanilla javascript. This project is still a work in progress. Currently working on making the ui responsive for mobile devices. Currently responsive for everything up to laptops! Have fun! ",
+      "The goal of the project was to focus on writing dense programming logic when I first started writing code as well as focus on heavy DOM manipulation by creating each individual card using BootStrap, CSS, vanilla javascript. This project is still a work in progress. Currently working on making the ui responsive for devices with a smaller screen size than a laptop. Have fun! ",
     deploy: "https://alexanderleino.github.io/Netflix-and-Grill/",
     repository: "https://github.com/AlexanderLeino/Netflix-and-Grill",
     icons: [<ImHtmlFive />, <SiJavascript />, <DiCss3 />, <BsBootstrap />],
@@ -46,7 +43,7 @@ export const projectsArray = [
   {
     title: "Your Local Farmer",
     description:
-      "This was the capstone project for my coding bootcamp. The goal of the project was to build a fullstack application within react. We build the backend using mongodb and used GraphQL to authenticate, render and save data. We also wanted to experiment with using a component library that we have never used before. We chose to use Chakra UI because it aheres to WAI-ARIA standards for accessiblity.",
+      "This was the capstone project for my coding bootcamp. The goal of the project was to build a fullstack application within react. We built the backend using mongodb and GraphQL to authenticate, render and save data. We also wanted to experiment with using a component library that we have never used before. We chose to use Chakra UI because it aheres to WAI-ARIA standards for accessiblity.",
     repository: "https://github.com/AlexanderLeino/Netflix-and-Grill",
     icons: [
       <ImHtmlFive />,
@@ -68,7 +65,7 @@ export const projectsArray = [
   {
     title: "My Portfolio",
     description:
-      "I built this portfolio to showcase my previous works. The Goal of the project was to create and implement a dark and light theme, I also wanted to use HashNodes api to get all of my previous blog posts and have them visible under the articles tab",
+      "I built this portfolio to showcase my previous works. The goal of the project was to create and implement a dark and light theme, I also wanted to use HashNode's API to get all of my previous blog posts and have them visible under the articles tab.",
     deploy: "https://alexanderleino.github.io/Netflix-and-Grill/",
     repository: "https://github.com/AlexanderLeino/Netflix-and-Grill",
     icons: [<ImHtmlFive />, <SiJavascript />, <DiCss3 />, <BsBootstrap />],
